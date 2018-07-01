c126c563d93056127627a50a1166e581aa62b94c
Stephen Selvaraj <stephenselvaraj@stephens-mbp.home>
2018-07-01 16:55:46 -0400

    Add button functionality added

--- Todolist/TodolistViewController.swift
+++ Todolist/TodolistViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodolistViewController: UITableViewController {
 
-    let itemArray = ["Milk", "Buy Eggs", "Bread"]
+    var itemArray = ["Milk", "Buy Eggs", "Bread"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,5 +49,41 @@ class TodolistViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UIT extField()
+        
+        let alert = UIAlertController(title: "Add New To Do list", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+        
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action )
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
